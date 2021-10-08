28179c1a01413970ce74ed1f8066172025e802d7
DittoGod <dittogod1@gmail.com>
2021-10-08 18:11:14 +0200

    started adding sample Recipe List

--- recipes/lib/recipe.dart
+++ recipes/lib/recipe.dart
@@ -7,7 +7,13 @@ class Recipe {
     this.label,
     this.imageUrl,
   );
-  //TODO: Add List<Recipe> here.
+
+  static List<Recipe> samples = [
+    Recipe('Spaghetti and Meatballs', 'assets/2126711929_ef763de2b3_w.jpg'),
+    Recipe('Tomato Soup', 'assets/27729023535_a57606c1be.jpg'),
+    Recipe('Grilled Cheese', 'assets/3187380632_5056654a19_b.jpg'),
+    Recipe('Chocolate Chip Cookies', 'assets/15992102771_b92f4cc00a_b.jpg'),
+  ];
 }
 
 //TODO: Add Ingredient() here
