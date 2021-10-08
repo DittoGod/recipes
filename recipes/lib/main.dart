db4ed1b8d93680476afc0c929959d5736bc9933c
DittoGod <dittogod1@gmail.com>
2021-10-08 21:43:38 +0200

    added gesture detector

--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -46,7 +46,15 @@ class _MyHomePageState extends State<MyHomePage> {
           itemCount: Recipe.samples.length,
           itemBuilder: (BuildContext context, int index) {
             // TODO: Add GestureDetector
-            return buildRecipeCard(Recipe.samples[index]);
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  // TODO: Replace return with return RecipeDetail()
+                  return Text('Detail Page');
+                }));
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
           },
         ),
       ),
