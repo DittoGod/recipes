83b95515b1569b93962ed07de4463a64c6c88135
DittoGod <dittogod1@gmail.com>
2021-10-09 11:29:50 +0200

    Added Recipe Detail page to Gesture detector ontap action

--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -47,10 +48,11 @@ class _MyHomePageState extends State<MyHomePage> {
           itemBuilder: (BuildContext context, int index) {
             return GestureDetector(
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  // TODO: Replace return with return RecipeDetail()
-                  return const Text('Detail Page');
-                }));
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return RecipeDetail(recipe: Recipe.samples[index]);
+                  },
+                ));
               },
               child: buildRecipeCard(Recipe.samples[index]),
             );
