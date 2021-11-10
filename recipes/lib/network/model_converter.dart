6b7db33fc75baa6dc2c9f421ba58c1706f27f71c
DittoGod <dittogod1@gmail.com>
2021-11-10 11:53:01 +0200

    Decoding JSON

--- recipes/lib/network/model_converter.dart
+++ recipes/lib/network/model_converter.dart
@@ -31,7 +31,33 @@ class ModelConverter implements Converter {
     return request;
   }
 
-  Response decodeJson<BodyType, InnerType>(Response response) {}
+  Response<BodyType> decodeJson<BodyType, InnerType>(Response response) {
+    final contentType = response.headers[contentTypeKey];
+    var body = response.body;
+    // 1
+    if (contentType != null && contentType.contains(jsonHeaders)) {
+      body = utf8.decode(response.bodyBytes);
+    }
+    try {
+      // 2
+      final mapData = json.decode(body);
+      // 3
+      if (mapData['status'] != null) {
+        return response.copyWith<BodyType>(
+            body: Error(Exception(mapData['status'])) as BodyType);
+      }
+      // 4
+      final recipeQuery = APIRecipeQuery.fromJson(mapData);
+      // 5
+      return response.copyWith<BodyType>(
+          body: Success(recipeQuery) as BodyType);
+    } catch (e) {
+      // 6
+      chopperLogger.warning(e);
+      return response.copyWith<BodyType>(
+          body: Error(e as Exception) as BodyType);
+    }
+  }
 
   @override
   Response<BodyType> convertResponse<BodyType, InnerType>(Response response) {}
